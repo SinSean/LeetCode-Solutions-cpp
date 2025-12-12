83af1ec1374255a1e954b117ae3beac862122941
AnnZi <56986861+SinSean@users.noreply.github.com>
2025-12-12 15:39:42 +0800

    Add files via upload

---
+++ 1523. Count Odd Numbers in an Interval Range.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int ans=0;
+        for(int i=low;i<=high;i++)
+        {
+            if(i%2==1)
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
+
+/*
+題目:
+求兩整數間的基數
+
+解法:
+區間可被mod=1就是基數
+用ans當計數器並回傳ans
+*/
